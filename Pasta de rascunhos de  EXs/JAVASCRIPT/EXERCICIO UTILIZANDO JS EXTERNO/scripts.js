51d8a81cb20f053d147e5a032020ebf69ae0cfeb
Felpasw <felipecavalcantelacerda@hotmail.com>
2022-05-19 16:02:19 -0300

    ex js externo adicionado

---
+++ Pasta de rascunhos de  EXs/JAVASCRIPT/EXERCICIO UTILIZANDO JS EXTERNO/scripts.js
@@ -0,0 +1,9 @@
+
+    alert("Bom dia porra");
+    var pMaisVelhaNome = prompt("Informe o nome da pessoa mais velha: ");
+    var pMaisVelhaIdade = prompt(" Informe a idade da pessoa mais velha: " + "(" + pMaisVelhaNome +")");
+    var pMaisNovaNome = prompt("Informe o nome da pessoa mais nova: ");
+    var pMaisNovaIdade= prompt("Informe a idade da pessoa mais nova: " + "(" + pMaisNovaNome +")");
+    confirm("Nome da pessoa mais velha: " + pMaisVelhaNome + "\n Idade da pessoa mais velha: "+ pMaisVelhaIdade);
+    confirm("Nome da pessoa mais nvoa: " + pMaisNovaNome + "\n Idade da pessoa mais nova: "+ pMaisNovaIdade);
+    alert("Diferença de idade: " + (pMaisVelhaIdade - pMaisNovaIdade))
