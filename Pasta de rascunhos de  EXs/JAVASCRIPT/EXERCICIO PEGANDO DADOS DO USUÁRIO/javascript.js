abab8ef0b8b8ca00fc53bf9be539818b4e528d90
Felpasw <felipecavalcantelacerda@hotmail.com>
2022-05-19 15:27:00 -0300

    exercício java adicionado

---
+++ Pasta de rascunhos de  EXs/JAVASCRIPT/EXERCICIO PEGANDO DADOS DO USUÁRIO/javascript.js
@@ -0,0 +1,9 @@
+function teste(){
+    
+    alert("Olá, vamos pegar algumas informações de você beleza? ");
+    var nome =  window.prompt("Informe seu nome: ");
+    var idade = window.prompt("Informe sua idade: ");
+    confirm("Então você possui " + idade + " anos certo?");
+    alert("Então você se chama  " + nome + "  e tem  " + idade + "  anos!!");
+
+} 
